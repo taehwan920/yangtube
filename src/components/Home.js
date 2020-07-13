d9ab9f638bb4d15eb36d92cb1aa5747065a65ca8
Yang Taehwan <taehwan920@gmail.com>
2020-07-13 20:59:20 +0900

    Basic css settings

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,26 @@ import Guide from './guideComponents/Guide';
 import Header from './headerComponents/Header';
 import MainFrame from './mainFrameComponents/MainFrame';
 import VirtualKeyboard from './VirtualKeyboard';
+import styled from 'styled-components';
+
+const HomeDiv = styled.div`
+    display: relative;
+    width: 100%;
+    height: 100%;
+    background-color: pink;
+`;
 
 export default class extends React.Component {
     render() {
         return (
-            <div>
-                <Header></Header>
-                <Guide></Guide>
-                <MainFrame></MainFrame>
-                <VirtualKeyboard></VirtualKeyboard>
-            </div>
+            <HomeDiv>
+                <div>
+                    <Header></Header>
+                    <Guide></Guide>
+                    <MainFrame></MainFrame>
+                    <VirtualKeyboard></VirtualKeyboard>
+                </div>
+            </HomeDiv>
         )
     }
 }
--- src/components/headerComponents/Header.js
+++ src/components/headerComponents/Header.js
@@ -4,33 +4,45 @@ import HeaderApps from './HeaderApps'
 import HeaderAddVideo from './HeaderAddVideo';
 import HeaderSearch from './HeaderSearch';
 import HeaderUserInfo from './HeaderUserInfo';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 15%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center; 
+    background-color: blue;
+`;
 
 export default class extends React.Component {
     render() {
         ChangeTitle('Main Page!');
         return (
-            <header className="header">
-                <div>
-                    <MenuButton></MenuButton>
-                    <Logo></Logo>
-                </div>
-                <HeaderSearch></HeaderSearch>
-                <div><i class="fas fa-search"></i></div>
-                <nav>
+            <HeaderWrapper>
+                <header className="header">
                     <div>
-                        <i class="fas fa-video"></i>
-                        <HeaderAddVideo></HeaderAddVideo>
+                        <MenuButton></MenuButton>
+                        <Logo></Logo>
                     </div>
-                    <div>
-                        <i class="fas fa-th"></i>
-                        <HeaderApps></HeaderApps>
-                    </div>
-                </nav>
-                <section>
-                    <div>G</div>
-                    <HeaderUserInfo></HeaderUserInfo>
-                </section>
-            </header>
+                    <HeaderSearch></HeaderSearch>
+                    <div><i class="fas fa-search"></i></div>
+                    <nav>
+                        <div>
+                            <i class="fas fa-video"></i>
+                            <HeaderAddVideo></HeaderAddVideo>
+                        </div>
+                        <div>
+                            <i class="fas fa-th"></i>
+                            <HeaderApps></HeaderApps>
+                        </div>
+                    </nav>
+                    <section>
+                        <div>G</div>
+                        <HeaderUserInfo></HeaderUserInfo>
+                    </section>
+                </header>
+            </HeaderWrapper>
         )
     }
 }
