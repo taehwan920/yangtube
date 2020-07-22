6de868a8551cc55f0a065f02bf28b163f098c7b8
Yang Taehwan <taehwan920@gmail.com>
2020-07-22 16:55:25 +0900

    Complete Show more Function 댓글 자세히 보기 기능 구현 완료

--- src/components/mainFrameComponents/commentsComponents/commentArticleComponents/CommentContent.js
+++ src/components/mainFrameComponents/commentsComponents/commentArticleComponents/CommentContent.js
@@ -5,12 +5,12 @@ import { commentItems } from '../CommentContents';
 const CommentContentWrapper = styled.div`
     width: 100%;
     height: auto;
+    position: relative;
 `;
 
 const CommentContent = styled.div`
     width: 100%;
-    height: auto;
-    min-height: 20px;
+    height: ${props => props.realHeight || 'auto'}px;
     font-size: 14px;
     white-space: pre-wrap;
     overflow: hidden;
@@ -20,23 +20,59 @@ const CommentContent = styled.div`
     text-overflow: '';
 `;
 
+const CommentHiddenBox = styled.div`
+    width: 100%;
+    height: auto;
+    font-size: 14px;
+    white-space: pre-wrap;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    opacity: 0;
+    z-index: -100;
+`;
+
 const CommentShowMore = styled.div`
     height: 16px;
     margin-top: 4px;
     font-size: 14px;
     color: rgba(0, 0, 0, 0.6);
+    cursor: pointer;
 `;
 
 export default class extends React.Component {
+    state = {
+        showMore: false,
+        realHeight: null
+    }
+
+    showToggle = () => {
+        const realHeight = document.querySelector('.hidden-box').clientHeight;
+        if (!this.state.showMore) {
+            this.setState({
+                showMore: true,
+                realHeight: realHeight
+            })
+        } else {
+            this.setState({
+                showMore: false,
+                realHeight: null
+            })
+        }
+    }
     render() {
+        const { realHeight } = this.state;
         return (
             <CommentContentWrapper>
-                <CommentContent>
+                <CommentContent realHeight={realHeight}>
                     {commentItems.main.content}
                 </CommentContent>
-                <CommentShowMore>
-                    자세히 보기
+                <CommentShowMore onClick={this.showToggle}>
+                    {realHeight ? '간략히' : '자세히 보기'}
                 </CommentShowMore>
+                <CommentHiddenBox
+                    className="hidden-box"
+                >{commentItems.main.content}</CommentHiddenBox>
             </CommentContentWrapper>
         )
     }
